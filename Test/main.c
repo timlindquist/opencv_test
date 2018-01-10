a0ce4f2dcdfc518263e3ccfabe9bf37225aafcd8
timlindquist <timmylindquist@gmail.com>
2018-01-10 09:58:23 -0600

    new

--- Test/main.c
+++ Test/main.c
@@ -1,15 +1,30 @@
-//
-//  main.c
-//  Test
-//
-//  Created by Tim Lindquist on 1/10/18.
-//  Copyright © 2018 Tim Lindquist. All rights reserved.
-//
-
 #include <stdio.h>
+#include <highgui.h>
 
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    printf("Hello, World!\n");
+int main(int argc, char *argv[])
+{
+    // load image from file
+    IplImage* img = NULL;
+    img = cvLoadImage("/Users/timlindquist/Desktop/cicada.jpg", CV_LOAD_IMAGE_UNCHANGED);
+    if (!img)
+    {
+        printf("Could not load image file: %s\n", argv[1]);
+        exit(0);
+    }
+    
+    // create window and show image
+    cvNamedWindow("mainWin", CV_WINDOW_NORMAL);
+    cvShowImage("mainWin", img);
+    
+    // workaround to send window to the top
+    cvMoveWindow("mainWin", 100, 100);
+    cvSetWindowProperty("mainWin", CV_WND_PROP_FULLSCREEN, CV_WINDOW_FULLSCREEN);
+    cvSetWindowProperty("mainWin", CV_WND_PROP_FULLSCREEN, CV_WINDOW_NORMAL);
+    
+    // free resources
+    cvReleaseImage(&img);
+    cvWaitKey(0);
+    cvDestroyWindow("mainWin");
     return 0;
 }
+
